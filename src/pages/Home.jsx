694bc0cd36a9172182bafcfc306d5cc79ae7295a
uttamthegoat <19516uttam@gmail.com>
2023-08-03 23:00:11 +0530

    Checked create post route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
 import axios from "../utils/axiosConfig";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  const [user, setUser] = useState("No user");
-  const [bio, setBio] = useState("");
+  const [post, setPost] = useState({ title: "", description: "", image: "" });
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
     const fetchUser = () => {
       axios
-        .get("/auth/getUserDetails")
+        .get("/posts/getPost/64cbbacbd74e9d60822d252e")
         .then((res) => {
-          setUser(res.data.user.name);
-          setBio(res.data.user.bio);
+          const { title, description, image } = res.data.post;
+          const { tags } = res.data;
+          setPost({ ...post, title, description, image });
+          setTags(tags)
         })
         .catch((error) => {
           if (error.response.data.status === "logout") navigate("/auth");
@@ -23,7 +25,19 @@ const Home = () => {
     fetchUser();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/add-post" className="block w-40 border px-3 py-2 bg-blue-600 text-white rounded-md">Create a new Blog</Link>
+      {post.title}
+      <img src={post.image} alt="postImage" />
+      <p>{post.description}</p>
+      {tags.map((tag,index)=>{
+        return (
+          <span key={index} className="bg-gray-600 px-2 py-1 rounded-2xl mx-0.5">{tag}</span>
+        )
+      })}
+    </div>
+  );
 };
 
 export default Home;
