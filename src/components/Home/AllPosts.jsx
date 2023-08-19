fde73c7bf86e95d831ec772decf4811eb7f95313
uttamthegoat <19516uttam@gmail.com>
2023-08-19 22:06:24 +0530

    Home page completed

--- src/components/Home/AllPosts.jsx
+++ src/components/Home/AllPosts.jsx
@@ -6,6 +6,7 @@ import BlogCard from "./BlogCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Spinner from "../static/Spinner";
 import axios from "../../utils/axiosConfig";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const AllPosts = () => {
   const navigate = useNavigate();
@@ -42,13 +43,25 @@ const AllPosts = () => {
 
   return (
     <div className="w-full px-2 sm:w-11/12 md:w-10/12 mx-auto mt-8">
-      <h1 className="text-4xl text-center font-bold mb-10">Blogs</h1>
+      <h1 className="text-4xl text-center font-bold mb-10 underline">Blogs</h1>
       <InfiniteScroll
         dataLength={allPosts.posts.length}
         next={fetchMore}
         hasMore={allPosts.posts.length !== allPosts.totalResults}
         loader={<Spinner />}
       >
+        {!allPosts.posts.length && (
+          <div>
+            <LazyLoadImage
+              src="https://img.memegenerator.net/instances/61821090.jpg"
+              alt="nigga?"
+              className="block mx-auto rounded-lg mb-2"
+            />
+            <p className="text-center text-2xl font-semibold">
+              Write some blogs nigga!
+            </p>
+          </div>
+        )}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 place-content-center">
           {allPosts.posts.map((blog) => {
             return (
