33db69903bdddae0313334d92f82f28e86355e5b
uttamthegoat <19516uttam@gmail.com>
2023-11-28 23:48:51 +0530

    Deployment ready

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-// const instance = axios.create({
-//   baseURL: "https://server-blog-app-delta.vercel.app/api/v1/blog-app",
-//   withCredentials: true,
-// });
-
 const instance = axios.create({
-  baseURL: "http://localhost:5002/api/v1/blog-app",
+  // baseURL: "http://localhost:5002/api/v1/blog-app",
+  baseURL: "https://server-blog-app-delta.vercel.app/api/v1/blog-app",
   withCredentials: true,
 });
 
